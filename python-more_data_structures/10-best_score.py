3c5a3df6e59f4f6fb53e6cb7272ebdd32ed293bd
Kellia855 <k.kamikazi@alustudent.com>
2025-02-19 09:04:49 -0800

    function that returns a key with the biggest integer value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    return {key: a_dictionary[key] * 2 for key in a_dictionary}
+def best_score(a_dictionary):
+    if isinstance(a_dictionary, dict) and len(a_dictionary) > 0:
+        return max(a_dictionary, key=a_dictionary.get)
+    return None
