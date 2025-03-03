05f9ddaecb3d8d9815eaffb17a065b7a26c53f66
Kellia855 <k.kamikazi@alustudent.com>
2025-03-03 03:48:33 -0800

    python-exceptions

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import sys
-
-
-def safe_function(fct, *args):
-    try:
-        output = fct(*args)
-    except Exception as E:
-        print("Exception: {}".format(E), file=sys.stderr)
-        return None
-    else:
-        return output
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return result
