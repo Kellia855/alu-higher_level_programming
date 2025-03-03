200f8bcd2cc29e2e5ebb9a9d14fc9cf278e4654f
Kellia855 <k.kamikazi@alustudent.com>
2025-03-03 09:04:12 -0800

    Singly linked list

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,60 @@
+class Node:
+    """Defines a node of a singly linked list."""
+    def __init__(self, data, next_node=None):
+        """Initializes a node with data and next_node."""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Retrieves the data."""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Sets the data, ensuring it is an integer."""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrieves the next node."""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Sets the next node, ensuring it is a Node object or None."""
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Defines a singly linked list."""
+    def __init__(self):
+        """Initializes an empty singly linked list."""
+        self.__head = None
+
+    def __str__(self):
+        """Returns a string representation of the list, one number per line."""
+        values = []
+        current = self.__head
+        while current is not None:
+            values.append(str(current.data))
+            current = current.next_node
+        return "\n".join(values)
+
+    def sorted_insert(self, value):
+        """Inserts a new Node into the correct sorted position in the list."""
+        new_node = Node(value)
+        if self.__head is None or self.__head.data >= value:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            current = self.__head
+            while current.next_node is not None and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
