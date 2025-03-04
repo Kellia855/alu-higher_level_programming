5b72f89b1f8e8c5699c3c5b983953cad286bb678
Kellia855 <k.kamikazi@alustudent.com>
2025-03-04 01:21:27 -0800

    Print Square instance

---
+++ python-classes/101-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the square with size and position."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size, ensuring it is an integer and >= 0."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieves the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position, ensuring it is a tuple of 2 positive integers."""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(i, int) and i >= 0 for i in value)):
+            raise TypeError("position must be a tuple of 2 positive integer")
+        self.__position = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with '#' and respects the position."""
+        if self.__size == 0:
+            print()
+            return
+        print("\n" * self.__position[1], end="")
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+    def __str__(self):
+        """Defines the string representation of the square."""
+        if self.__size == 0:
+            return ""
+        result = "\n" * self.__position[1]
+        result += "\n".join(" " * self.__position[0] + "#" * self.__size
+                             for _ in range(self.__size))
+        return result
