83e933acf59a4008d78f101a1283d57c6e97105b
Kellia855 <k.kamikazi@alustudent.com>
2025-03-12 04:38:03 -0700

    function that reads a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,4 +7,5 @@ text file.
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its contents to stdout."""
     with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+        print(file.read(), end=")
+
