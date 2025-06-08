531585c3c1b012616dc4376e617c3213f4aa177f
Kellia855 <k.kamikazi@alustudent.com>
2025-06-08 10:45:13 +0000

    working with web scraping

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+t fs = require('fs');
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFileSync(filePath, stringToWrite, 'utf-8', function (error) {
+  if (error) {
+    console.error(error);
+  }
+});
---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+t request = require('request');
+const url = process.argv[2];
+
+request.get(url, (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`code: ${res.statusCode}`);
+});
+
